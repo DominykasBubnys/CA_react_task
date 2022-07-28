a17e65dc6156e7bc5cdc9a95b3584aafcdc073c2
dominykas <bubnys.it@gmail.com>
2022-07-28 20:04:53 +0300

    initializing app and adding routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+
+import Home from "./Pages/Home";
+
+const App = () => {
+
+  const routes = <Routes>
+    <Route path="/" element={<Home />}></Route>
+    <Route path="*" element={<Home />}></Route>
+  </Routes>
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main>{routes}</main>
+      <h1 style={ {color:"red"}}>Hello</h1>
+    </Router>
   );
 }
 
